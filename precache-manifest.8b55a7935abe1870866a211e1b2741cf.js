1dadb90065660f8f518f0d465c7463d56f4f9e55
Angelica Grace Aldea Carrasco <angiccarrasco@gmail.com>
2020-10-02 15:23:21 +0800

    Updates

---
+++ precache-manifest.8b55a7935abe1870866a211e1b2741cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a88654c64112010d9630175e7eddefba",
+    "url": "/authentication-app/index.html"
+  },
+  {
+    "revision": "1e6a3075ba96a70863e3",
+    "url": "/authentication-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "14111e378a963223368c",
+    "url": "/authentication-app/static/js/2.09ce1eee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/authentication-app/static/js/2.09ce1eee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e6a3075ba96a70863e3",
+    "url": "/authentication-app/static/js/main.233efc7d.chunk.js"
+  },
+  {
+    "revision": "213a0d6805399d70142e",
+    "url": "/authentication-app/static/js/runtime-main.bb47a171.js"
+  }
+]);
