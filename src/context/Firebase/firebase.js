493b7d87e208dbf70269daa4d8d880c3bcf4f0c5
Angelica Grace Aldea Carrasco <angiccarrasco@gmail.com>
2020-10-03 19:07:14 +0800

    Setup firebase database

--- src/context/Firebase/firebase.js
+++ src/context/Firebase/firebase.js
@@ -1,5 +1,6 @@
 import app from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/database';
 
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -15,6 +16,7 @@ class Firebase {
     app.initializeApp(config);
 
     this.auth = app.auth();
+    this.db = app.database();
   }
 
   // Auth API
@@ -25,6 +27,11 @@ class Firebase {
     this.auth.signInWithEmailAndPassword(email, password);
 
   doSignOut = () => this.auth.signOut();
+
+  // User API
+  getUserWithUid = (uid) => this.db.ref(`users/${uid}`);
+
+  getUsers = () => this.db.ref('users');
 }
 
 export default Firebase;
