2f16623ad8f47f55a7863b1217b399e08427f174
Angelica Grace Aldea Carrasco <angiccarrasco@gmail.com>
2020-10-03 18:15:11 +0800

    Add authorization on Account page

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,6 +1,19 @@
 import React from 'react';
-import { Text } from '@chakra-ui/core';
+import { Flex, Text } from '@chakra-ui/core';
 
-const Account = () => <Text>Account</Text>;
+import { AuthUserContext, withAuthorization } from '../../context/Session';
 
-export default Account;
+const Account = () => (
+  <AuthUserContext.Consumer>
+    {(authUser) => (
+      <Flex direction='column'>
+        <Text>Personal Info</Text>
+        <Text>{authUser.email}</Text>
+      </Flex>
+    )}
+  </AuthUserContext.Consumer>
+);
+
+const condition = (authUser) => !!authUser;
+
+export default withAuthorization(condition)(Account);
